a481d92dce12603ca8d2eb39c74232b8195dab10
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-05 10:52:49 -0300

    created urls for each request

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
+server.use(cors());
+
+server.post("/sign-up", (req, res) => {
+
+});
+
+server.post("/tweets", (req, res) =>{
+
+});
+
+server.get("/tweets", (req, res) => {
+
+});
+
+server.listen(5000, (req, res) => {
+    console.log("Backend Inicializado!!🚀");
+});
