e65ae00ddcf662f997b133cc2570bd582441abac
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-06 16:42:39 -0300

    return the last ten tweets

--- src/app.js
+++ src/app.js
@@ -20,14 +20,14 @@ server.post("/tweets", (req, res) =>{
 
     if (isSigned(data.username)) {
         tweets.push(data);
+        console.log(tweets);
         msg = "OK";
     }
-
     res.send(msg);
 });
 
 server.get("/tweets", (req, res) => {
-
+    res.send(buildTweets());
 });
 
 server.listen(5000, (req, res) => {
@@ -35,11 +35,41 @@ server.listen(5000, (req, res) => {
 });
 
 function isSigned(user){
-    users.forEach((u) => {
-        if (u.username === user) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === user) {
             return true;
         }
-    });
+    }
 
     return false;
+}
+
+function returnUserAvatar(user){
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === user) {
+            return users[i].avatar;
+        }
+    }
+
+    return "";
+}
+
+function buildTweets(){
+    const newArr = [];
+
+    if (tweets.length === 0) {
+        return [];
+    }
+
+    for (let i = 0; i < 10 && i < tweets.length; i++) {
+        const tweetData = {
+            username: tweets[tweets.length - (i + 1)].username,
+            avatar: returnUserAvatar(tweets[tweets.length - (i + 1)].username),
+            tweet: tweets[tweets.length - (i + 1)].tweet,
+        };
+        
+        newArr.push(tweetData);
+    }
+    console.log(newArr);
+    return newArr;
 }
